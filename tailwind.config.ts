57d01ba8731ae7da47438c04330ce8150ff26150
reza <preza7020@gmail.com>
2024-06-01 13:29:56 +0700

    chore: configurasi font family and colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,54 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        brand: {
+          main: "18A800",
+          dark: "0C5400",
+          light: "72E048",
+          subtle: "D4F5C8",
+        },
+        state: {
+          error: {
+            main: "FF3B3B",
+            dark: "E53535",
+            light: "FF5C5C",
+            subtle: "FF8080",
+          },
+          warning: {
+            main: "FFCC00",
+            dark: "E5B800",
+            light: "FDDD48",
+            subtle: "FDED72",
+          },
+          info: {
+            main: "0063F7",
+            dark: "004FC4",
+            light: "5B8DEF",
+            subtle: "9DBFF9",
+          },
+          success: {
+            main: "06C270",
+            dark: "05A660",
+            light: "39D98A",
+            subtle: "57EBA1",
+          },
+        },
+        dark: {
+          main: "3A3A3C",
+          dark: "6B7588",
+          light: "8F90A6",
+          subtle: "C7C9D9",
+        },
+        light: {
+          main: "DDE5E9",
+          dark: "EBEBF0",
+          light: "F2F2F5",
+          subtle: "FAFAFC",
+        },
       },
     },
   },
