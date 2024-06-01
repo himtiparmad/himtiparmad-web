1c198c4f444c904f5e7b728473a43911dd88f333
reza <preza7020@gmail.com>
2024-06-01 14:51:10 +0700

    feat: slicing hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
 import Image from "next/image";
-
 import Hero from "@/assets/images/hero.png";
+import NavigationBar from "@/components/navigation-bar";
 
 export default function Home() {
   return (
     <div>
-      <div>
+      <NavigationBar />
+      <div className="relative -mt-24">
         <Image
           src={Hero}
           width={1360}
           height={900}
           alt="hero"
-          className="w-full -z-10"
+          className="w-full"
         />
       </div>
     </div>
